0d3b1f9b7351ed82fe5144f49c4459740c4141ca
NateBalducciSound <natejbal@gmail.com>
2026-01-20 14:10:05 -0800

    Add Unity Files
    Initialize Branch

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,46 @@
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    // Might need to workshop for later implementation
+    private RectTransform rectTransform;
+    private Image grabbableObject;
+
+    private Canvas canvas;
+
+// all "drag" are implemented to not throw errors
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        grabbableObject.color = new Color32 (255,255,255,170);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //using this one because final won't have mouse location
+       rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        //or
+
+        // transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        grabbableObject.color = new Color32 (255, 255, 255, 255);
+    }
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        grabbableObject = GetComponent<Image>();
+        canvas = GetComponentInParent<Canvas>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ScannerLoad(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    // Add get component later for Number of objects to track CPU usage and make sure no memory leaks happen from
+    // recurring animations
+    Debug.Log("Scanner Running: " + "INSERT ITEM NUMBER HERE");
+        
+    
+    }
+    public void QuitButton()
+    {   
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+}
